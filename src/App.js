866368f7c802161d58b662a98064bf94da0b88ec
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-11 14:40:32 +0100

    All Routes Implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  Private,
+} from './pages'
+
+import styled from 'styled-components'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/cart'>
+          <Cart />
+        </Route>
+        <Route exact path='/products'>
+          <Products />
+        </Route>
+        <Route exact path='/product/:id' children={<SingleProduct />} />
+        <Route exact path='/checkout'>
+          <Checkout />
+        </Route>
+        <Route to='*'>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
