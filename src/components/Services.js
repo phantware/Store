2ea1fb4b9f7530ae06dc12175925be7ff9807c9a
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-13 11:48:59 +0100

    Home page Implemented with Services component

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,36 @@ import styled from 'styled-components'
 import { services } from '../utils/constants'
 
 const Services = () => {
-  return <h4>services </h4>
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <article className='header'>
+          <h3>
+            custom furniture <br /> built only for you
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus rerum
+            illo doloribus corporis reprehenderit asperiores cupiditate dolorum
+            aliquid error, blanditiis quam veniam culpa atque aperiam itaque
+            inventore laborum at similique quaerat mollitia velit aliquam
+            ratione odit? Distinctio ipsa aut rerum.
+          </p>
+        </article>
+        <div className='services-center'>
+          {services.map((service) => {
+            const { id, icon, title, text } = service
+            return (
+              <article key={id} className='service'>
+                <span className='icon'>{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
