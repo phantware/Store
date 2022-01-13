85504f28f0c6c43815b8912a5b61f001ec2c983e
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-13 23:16:49 +0100

    FetchSingleProduct Implemented

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -19,6 +19,9 @@ const initialState = {
   products_error: false,
   products: [],
   featured_products: [],
+  single_products_loading: false,
+  single_products_error: false,
+  single_products: {},
 }
 
 const ProductsContext = React.createContext()
@@ -44,6 +47,16 @@ export const ProductsProvider = ({ children }) => {
     }
   }
 
+  const fetchSingleProduct = async () => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN })
+    try {
+      const response = await axios.get(url)
+      const singleProduct = response.data
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct })
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR })
+    }
+  }
   useEffect(() => {
     fetchProducts(url)
   }, [])
