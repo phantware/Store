b6d8aa708b9bf0d7136f928d301f9e145fc675cd
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-13 11:55:17 +0100

    Home page Implemented with Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,32 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+            asperiores ipsam, vel corrupti natus nobis, non iusto assumenda ipsa
+            magni consequatur ipsum veniam facere ratione maxime porro
+            reiciendis impedit, ullam at voluptatibus obcaecati consectetur
+            exercitationem molestias. Quae nam libero ut!
+          </p>
+          <form className='contact-form'>
+            <input
+              type='email'
+              className='form-input'
+              placeholder='enter email'
+            />
+            <button type='submit' className='submit-btn'>
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
