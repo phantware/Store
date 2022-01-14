dccc31cd5b88793fb6052e97271b3a6545179ee4
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-14 17:19:05 +0100

    Add to Cart Implemented

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -10,6 +10,26 @@ const AddToCart = ({ product }) => {
   const { id, stock, colors } = product
 
   const [mainColor, setMainColor] = useState(colors[0])
+  const [amount, setAmount] = useState(1)
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1
+      if (tempAmount > stock) {
+        return stock
+      }
+      return tempAmount
+    })
+  }
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1
+      if (tempAmount < 1) {
+        tempAmount = 1
+      }
+      return tempAmount
+    })
+  }
 
   return (
     <Wrapper>
@@ -31,6 +51,16 @@ const AddToCart = ({ product }) => {
             )
           })}
         </div>
+      </div>
+      <div className='btn-container'>
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
+        <Link to='/cart' className='btn'>
+          add to cart
+        </Link>
       </div>
     </Wrapper>
   )
