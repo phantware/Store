2946e7d202dd552771cfd0258e4ecbd816ac7468
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-22 19:38:28 +0100

    Environment varible implemented with strip

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
   <Auth0Provider
-    domain='dev-h1bfgeu3.us.auth0.com'
-    clientId='sUfKL9715fZKt7AUvc7OZwxHwYr3Fhny'
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation='localstorage'
   >
