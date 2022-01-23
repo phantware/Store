528c6eb9eda479978e981bfae10ce5af963ce6a2
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-23 08:33:25 +0100

    Checkout payment - get client secret implemented

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -50,10 +50,12 @@ const CheckoutForm = () => {
 
   const createPaymentIntent = async () => {
     try {
-      const data = axios.post(
+      const { data } = await axios.post(
         '/.netlify/functions/create-payment-intent',
         JSON.stringify({ cart, shipping_fee, total_amount })
       )
+
+      setClientSecret(data.clientSecret)
     } catch (err) {}
   }
 
